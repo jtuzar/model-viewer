fee5ff370d2b0752331b391411eeb1c922f35ab3
Jakub Tuzar <jakub@tuzar.me>
2026-01-10 22:40:35 +0100

    window being opened

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,39 @@
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
 #include <iostream>
-#include <test.hpp>
 
-int main() {
-  std::cout << "Hello World!" << std::endl;
-  int result = add(2, 5);
-  std::cout << "Result is: " << result << std::endl;
+int main(void) {
+    GLFWwindow *window;
+
+    if (!glfwInit()) {
+        std::cout << "GLFW initialization error" << std::endl;
+        return -1;
+    }
+
+    window = glfwCreateWindow(640, 640, "Hello World", NULL, NULL);
+
+    if (!window) {
+        std::cout << "Failed to create glfw window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    int version = gladLoadGL(glfwGetProcAddress);
+    if (version == 0) {
+        std::cout << "Failed to initialize OpenGL context" << std::endl;
+        return -1;
+    }
+
+    std::cout << "Loaded OpenGL " << GLAD_VERSION_MAJOR(version) << "."
+              << GLAD_VERSION_MINOR(version) << std::endl;
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
 }
